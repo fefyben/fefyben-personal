27035437f9df2e45872efbbd3c3328e3798024a9
fbenites <fbenites@hartinc.com>
2019-04-04 11:53:32 -0400

    Update project

--- app/js/main.js
+++ app/js/main.js
@@ -1,14 +1,28 @@
-'use strict';
+(function() {
+  'use strict';
+  
+  const aboutContent = document.querySelector('#about-content'),
+      skillsContent = document.querySelector('#skillsbar-content');
 
-(function getSkills() {
-  let item = '';
-
-  fetch('content/skills.json')
+  fetch('content/content.json')
     .then(res => res.json())
     .then(data => {
-      const entries = Object.entries(data);
+      console.log(data);
 
-      for (const [lang, percent] of entries) {
+      // Get About content
+      aboutContent.innerHTML = `<p>${ data.about.content.replace(/\n/g, '</p><p>') }</p>`;
+
+      // Get Portfolio content - TODO
+
+      // Get Skills content
+      const skillsbarList = document.createElement('ul');
+      skillsbarList.classList.add('skillsbar');
+
+      const skillsData = Object.entries(data.skillset.skillsList);
+      
+      let item = '';
+
+      for (const [lang, percent] of skillsData) {
         let num = parseInt(percent);
         num = Math.round(num / 10);
 
@@ -24,25 +38,44 @@
           </li>
         `;
         
-        document.querySelector('.skillsbar').innerHTML = item;
+        skillsbarList.innerHTML = item;
       }
+
+      skillsContent.appendChild(skillsbarList);
+
+      // Get Contact content - TODO
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log(err)); // TODO
+
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
 })();
 
 // Load animation when skills is in Viewport
-const isInViewport = elem => {
-  const distance = elem.getBoundingClientRect();
-	return (
-		distance.top >= 0 &&
-		distance.left >= 0 &&
-		distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-		distance.right <= (window.innerWidth || document.documentElement.clientWidth)
-	);
-};
+window.addEventListener('scroll', () => {
+  const isInViewport = elem => {
+    const distance = elem.getBoundingClientRect();
+    return (
+      distance.top >= 0 &&
+      distance.left >= 0 &&
+      distance.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      distance.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
 
-window.addEventListener('scroll', function(e) {
-  let skills = document.querySelectorAll('.skillsbar li');
+  const skills = document.querySelectorAll('.skillsbar li');
 
   skills.forEach(skill => {
     let skillPercent = skill.children[0].children[1].textContent;
@@ -50,21 +83,12 @@ window.addEventListener('scroll', function(e) {
 
     let skillItem = skill.children[1].children[0];
 
-	  if (isInViewport(document.querySelector('.skillset li:first-child')) || isInViewport(document.querySelector('.skillset li:last-child'))) {
+    if (isInViewport(document.querySelector('.skillset li:first-child')) || 
+        isInViewport(document.querySelector('.skillset li:last-child'))) {
       skillItem = skillItem.className = `skillsbar-bar skillsbar-${ skillPercent }`;
     } else {
       skillItem = skillItem.classList.remove(`skillsbar-${ skillPercent }`);
     }
   });
-}, false);
+});
 
-// document.addEventListener("DOMContentLoaded", getAbout());
-// Get About content
-window.onload = function getAbout() {
-  fetch('content/about.txt')
-    .then(res => res.text())
-    .then(data => {
-      document.querySelector('#about-content').innerHTML = data;
-    })
-    .catch(err => console.log(err));
-};
